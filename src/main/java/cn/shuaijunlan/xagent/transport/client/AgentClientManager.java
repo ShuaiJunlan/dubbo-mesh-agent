79bfdc425b85e0b3a7b8321de7937cd20ed5e2ca
shuaijunlan <shuaijunlan@gmail.com>
2018-05-03 09:01:10 +0800

    update

--- src/main/java/cn/shuaijunlan/xagent/transport/client/AgentClientManager.java
+++ src/main/java/cn/shuaijunlan/xagent/transport/client/AgentClientManager.java
@@ -1,53 +1,57 @@
 package cn.shuaijunlan.xagent.transport.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.LinkedList;
-import java.util.List;
 
 /**
  * @author Junlan Shuai[shuaijunlan@gmail.com].
  * @date Created on 20:47 2018/5/2.
  */
 public class AgentClientManager {
+    private static Logger logger = LoggerFactory.getLogger(AgentClientManager.class);
     //考虑优先级队列
 
     private static LinkedList<AgentClient> agentClients = new LinkedList<>();
 
     private static Object object = new Object();
+    static{
+        add();
+    }
 
     /**
      * 获取连接实例
      * @return
      */
-    public synchronized static AgentClient getAgentClientInstance(){
+    public static AgentClient getAgentClientInstance(){
         synchronized (object){
             if (agentClients.isEmpty()){
                 add();
             }
         }
-
+        if (logger.isInfoEnabled()){
+            logger.info("Execute getAgentClientInstance, AgentClientManager Size:{}", agentClients.size());
+        }
         return agentClients.pop();
     }
     public static void putOne(AgentClient client){
         synchronized (object){
             agentClients.add(client);
         }
+        if (logger.isInfoEnabled()){
+            logger.info("Execute putOne, AgentClientManager Size:{}", agentClients.size());
+        }
     }
 
     private static void add(){
-        for (int i = 0; i < 80; i++){
-            AgentClient client = new AgentClient("127.0.0.1", 1234);
+        for (int i = 0; i < 240; i++){
+            AgentClient client = new AgentClient("127.0.0.1", 1234 + (i%3));
             client.start();
             agentClients.add(client);
         }
-        for (int i = 0; i < 80; i++){
-            AgentClient client = new AgentClient("127.0.0.1", 1235);
-            client.start();
-            agentClients.add(client);
-        }
-        for (int i = 0; i < 80; i++){
-            AgentClient client = new AgentClient("127.0.0.1", 1236);
-            client.start();
-            agentClients.add(client);
+        if (logger.isInfoEnabled()){
+            logger.info("Execute add(), AgentClientManager Size:{}", agentClients.size());
         }
     }
 
