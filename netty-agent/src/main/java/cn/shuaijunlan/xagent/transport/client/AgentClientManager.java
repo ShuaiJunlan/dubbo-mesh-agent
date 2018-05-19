78b4243d9494c165a8e5d4221664776a4de1ef60
shuaijunlan <shuaijunlan@gmail.com>
2018-05-19 19:26:12 +0800

    update

--- netty-agent/src/main/java/cn/shuaijunlan/xagent/transport/client/AgentClientManager.java
+++ netty-agent/src/main/java/cn/shuaijunlan/xagent/transport/client/AgentClientManager.java
@@ -1,8 +1,12 @@
 package cn.shuaijunlan.xagent.transport.client;
 
+import cn.shuaijunlan.xagent.registry.Endpoint;
+import cn.shuaijunlan.xagent.registry.EtcdRegistry;
+import cn.shuaijunlan.xagent.registry.IRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -16,9 +20,15 @@ public class AgentClientManager {
 
     private static ConcurrentHashMap<Integer, AgentClient> agentClients = new ConcurrentHashMap<>();
     private static AtomicInteger atomicInteger = new AtomicInteger(0);
-
+    private static List<Endpoint> endpoints = null;
+    private static IRegistry registry = new EtcdRegistry(System.getProperty("etcd.url"));
     private static Object object = new Object();
     static{
+        try {
+            endpoints = registry.find("");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         add();
     }
 
@@ -28,52 +38,21 @@ public class AgentClientManager {
      */
     public static AgentClient getAgentClientInstance(){
 
-        AgentClient agentClient = agentClients.get(atomicInteger.getAndIncrement() % 480);
+        AgentClient agentClient = agentClients.get(0);
         if (agentClient.channel != null && agentClient.channel.isActive()){
             return agentClient;
         }else {
             logger.info("channel isn't avaliable");
             return  getAgentClientInstance();
         }
-//        agentClients.add(agentClient);
-//        if (logger.isInfoEnabled()){
-//            logger.info("Execute getAgentClientInstance, AgentClientManager Size:{}", agentClients.size());
-//        }
+
     }
-//    public static void putOne(AgentClient client){
-//        synchronized (object){
-//            agentClients.add(client);
-//        }
-//        if (logger.isInfoEnabled()){
-//            logger.info("Execute putOne, AgentClientManager Size:{}", agentClients.size());
-//        }
-//    }
 
     private static void add(){
-        for (int i = 0; i < 80; i++){
-            cn.shuaijunlan.xagent.transport.client.AgentClient client1 = new cn.shuaijunlan.xagent.transport.client.AgentClient("127.0.0.1", 1234);
+        for (int i = 0; i < 3; i++){
+            AgentClient client1 = new AgentClient(endpoints.get(i).getHost(), endpoints.get(i).getPort());
             client1.start();
-            agentClients.put(6*i + 0, client1);
-
-            cn.shuaijunlan.xagent.transport.client.AgentClient client2 = new cn.shuaijunlan.xagent.transport.client.AgentClient("127.0.0.1", 1235);
-            client2.start();
-            agentClients.put(6*i + 1, client2);
-
-            cn.shuaijunlan.xagent.transport.client.AgentClient client3 = new cn.shuaijunlan.xagent.transport.client.AgentClient("127.0.0.1", 1235);
-            client3.start();
-            agentClients.put(6*i + 2, client3);
-
-            cn.shuaijunlan.xagent.transport.client.AgentClient client4 = new cn.shuaijunlan.xagent.transport.client.AgentClient("127.0.0.1", 1236);
-            client4.start();
-            agentClients.put(6*i + 3, client4);
-
-            cn.shuaijunlan.xagent.transport.client.AgentClient client5 = new cn.shuaijunlan.xagent.transport.client.AgentClient("127.0.0.1", 1236);
-            client5.start();
-            agentClients.put(6*i + 4, client5);
-
-            cn.shuaijunlan.xagent.transport.client.AgentClient client6 = new AgentClient("127.0.0.1", 1236);
-            client6.start();
-            agentClients.put(6*i + 5, client6);
+            agentClients.put(i, client1);
 
         }
     }
