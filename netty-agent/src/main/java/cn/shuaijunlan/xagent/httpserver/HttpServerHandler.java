30dbdbf57330c383f45bc1e47eae79b2d8759a2b
shuaijunlan <shuaijunlan@gmail.com>
2018-05-21 20:00:21 +0800

    update

--- netty-agent/src/main/java/cn/shuaijunlan/xagent/httpserver/HttpServerHandler.java
+++ netty-agent/src/main/java/cn/shuaijunlan/xagent/httpserver/HttpServerHandler.java
@@ -85,7 +85,7 @@ public class HttpServerHandler extends ChannelInboundHandlerAdapter {
                     /////////////////////////////////////////////////////////////
 
                     ////////////////////////////////////////////////////////////////////////////
-                    String url = "http://" + HttpSnoopServer.endpoints.get(2).getHost() + ":" + HttpSnoopServer.endpoints.get(2).getPort();
+                    String url = "http://" + "10.10.10.5" + ":" + 30000;
                     logger.info("Request url:{}", url);
                     Result result = new Result();
 
