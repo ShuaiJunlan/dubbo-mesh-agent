caa137c506e28c5c8bf021a4ab82a6cec5468feb
shuaijunlan <shuaijunlan@gmail.com>
2018-05-21 22:16:10 +0800

    update

--- netty-agent/src/main/java/cn/shuaijunlan/xagent/httpserver/HttpSnoopServer.java
+++ netty-agent/src/main/java/cn/shuaijunlan/xagent/httpserver/HttpSnoopServer.java
@@ -44,7 +44,7 @@ public class HttpSnoopServer {
         if (type != null && type.equals("client")){
             // Configure the server.
             EventLoopGroup bossGroup = new EpollEventLoopGroup();
-            EventLoopGroup workerGroup = new EpollEventLoopGroup(4);
+            EventLoopGroup workerGroup = new EpollEventLoopGroup();
             try {
                 ServerBootstrap b = new ServerBootstrap();
                 b.group(bossGroup, workerGroup)
