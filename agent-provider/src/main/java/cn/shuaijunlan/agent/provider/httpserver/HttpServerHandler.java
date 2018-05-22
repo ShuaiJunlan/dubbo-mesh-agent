54dba5d4eeb93c8ca5522e65ceda4fa1ca510dd4
shuaijunlan <shuaijunlan@gmail.com>
2018-05-22 20:23:01 +0800

    update

--- agent-provider/src/main/java/cn/shuaijunlan/agent/provider/httpserver/HttpServerHandler.java
+++ agent-provider/src/main/java/cn/shuaijunlan/agent/provider/httpserver/HttpServerHandler.java
@@ -39,6 +39,7 @@ public class HttpServerHandler extends ChannelInboundHandlerAdapter {
             FullHttpRequest req = (FullHttpRequest) msg;
             // 将耗时任务交给任务线程池处理
             ctx.executor().execute(() -> {
+                long start = System.currentTimeMillis();
                 //执行远程调用
                 String[] tmp = req.uri().split("&parameter=");
                 String str = "";
@@ -71,6 +72,8 @@ public class HttpServerHandler extends ChannelInboundHandlerAdapter {
                 } else {
                     ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
                 }
+                long end = System.currentTimeMillis();
+                logger.info("Get response from provider service spending {}ms!", end-start);
             });
         }else {
             FullHttpResponse response = new DefaultFullHttpResponse(
