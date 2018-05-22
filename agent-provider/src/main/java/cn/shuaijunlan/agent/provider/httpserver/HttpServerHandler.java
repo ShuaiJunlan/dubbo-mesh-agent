d8410ad32298e10320eb6525e50aa9148102e8e9
shuaijunlan <shuaijunlan@gmail.com>
2018-05-22 14:50:58 +0800

    update

--- agent-provider/src/main/java/cn/shuaijunlan/agent/provider/httpserver/HttpServerHandler.java
+++ agent-provider/src/main/java/cn/shuaijunlan/agent/provider/httpserver/HttpServerHandler.java
@@ -61,9 +61,6 @@ public class HttpServerHandler extends ChannelInboundHandlerAdapter {
 
                     if (result != null){
                         integer = new String((byte[]) result).replaceFirst("\n", "");
-                        if (Integer.valueOf(integer) != str.hashCode()){
-                            logger.info("The hashcode is not equal. Expected:{}, Actual:{}", str.hashCode(), integer);
-                        }
                     }else {
                         logger.info("The return value of provider service is null!");
                     }
