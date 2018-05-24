f7dd80371831381c9a84f07e933056cab465dec9
shuaijunlan <shuaijunlan@gmail.com>
2018-05-24 17:19:59 +0800

    update

--- agent-provider/src/main/java/cn/shuaijunlan/agent/provider/dubbo/model/ConnectionHolder.java
+++ agent-provider/src/main/java/cn/shuaijunlan/agent/provider/dubbo/model/ConnectionHolder.java
@@ -1,14 +1,19 @@
 package cn.shuaijunlan.agent.provider.dubbo.model;
 
 import cn.shuaijunlan.agent.provider.dubbo.ConnectionManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Junlan Shuai[shuaijunlan@gmail.com].
  * @date Created on 16:31 2018/5/24.
  */
 public class ConnectionHolder {
+    private static Logger logger = LoggerFactory.getLogger(ConnectionHolder.class);
+    private static AtomicInteger atomicInteger = new AtomicInteger(0);
     private static LinkedList<ConnectionManager> connectionManagers = new LinkedList<>();
     public static synchronized ConnectionManager getConnectionManager(){
         while (connectionManagers.isEmpty()){
@@ -22,6 +27,7 @@ public class ConnectionHolder {
 
     private static boolean newConnectionManager(){
         ConnectionManager c = new ConnectionManager();
+        logger.info("Creating a new connection: {}!", atomicInteger.incrementAndGet());
         return connectionManagers.add(c);
     }
 
