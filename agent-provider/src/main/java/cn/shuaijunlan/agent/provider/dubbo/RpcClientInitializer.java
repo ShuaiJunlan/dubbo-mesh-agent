a6f6853eeff3a2bbd2c04391cafd2ef5cda77874
shuaijunlan <shuaijunlan@gmail.com>
2018-05-24 11:33:46 +0800

    update

--- agent-provider/src/main/java/cn/shuaijunlan/agent/provider/dubbo/RpcClientInitializer.java
+++ agent-provider/src/main/java/cn/shuaijunlan/agent/provider/dubbo/RpcClientInitializer.java
@@ -8,15 +8,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Junlan
  */
 public class RpcClientInitializer extends ChannelInitializer<SocketChannel> {
+    private static AtomicInteger atomicInteger = new AtomicInteger(0);
     private Logger logger = LoggerFactory.getLogger(RpcClientInitializer.class);
     @Override
     protected void initChannel(SocketChannel socketChannel) {
-        logger.info("RpcClientInitializer initChannel!");
+        logger.info("RpcClientInitializer initChannel {}!", atomicInteger.incrementAndGet());
         ChannelPipeline pipeline = socketChannel.pipeline();
         //设置连接空闲时间
         pipeline.addLast(new IdleStateHandler(0, 20, 0, TimeUnit.SECONDS));
