75907b10c0d475d72e16232822d8ecd7fef4469f
shuaijunlan <shuaijunlan@gmail.com>
2018-05-24 11:20:18 +0800

    update

--- agent-provider/src/main/java/cn/shuaijunlan/agent/provider/dubbo/RpcClientInitializer.java
+++ agent-provider/src/main/java/cn/shuaijunlan/agent/provider/dubbo/RpcClientInitializer.java
@@ -4,6 +4,8 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.timeout.IdleStateHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,8 +13,10 @@ import java.util.concurrent.TimeUnit;
  * @author Junlan
  */
 public class RpcClientInitializer extends ChannelInitializer<SocketChannel> {
+    private Logger logger = LoggerFactory.getLogger(RpcClientInitializer.class);
     @Override
     protected void initChannel(SocketChannel socketChannel) {
+        logger.info("RpcClientInitializer initChannel!");
         ChannelPipeline pipeline = socketChannel.pipeline();
         //设置连接空闲时间
         pipeline.addLast(new IdleStateHandler(0, 20, 0, TimeUnit.SECONDS));
