61a9b98fc308c0c1997db094a7fca63b330ef4af
shuaijunlan <shuaijunlan@gmail.com>
2018-05-24 09:54:58 +0800

    update

--- agent-provider/src/main/java/cn/shuaijunlan/agent/provider/dubbo/RpcClientHandler.java
+++ agent-provider/src/main/java/cn/shuaijunlan/agent/provider/dubbo/RpcClientHandler.java
@@ -6,11 +6,31 @@ import cn.shuaijunlan.agent.provider.dubbo.model.RpcRequestHolder;
 import cn.shuaijunlan.agent.provider.dubbo.model.RpcResponse;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Junlan
  */
 public class RpcClientHandler extends SimpleChannelInboundHandler<RpcResponse> {
+    private Logger logger = LoggerFactory.getLogger(RpcClientHandler.class);
+    private static AtomicInteger atomicInteger = new AtomicInteger(0);
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object obj) throws Exception {
+        if (obj instanceof IdleStateEvent) {
+            IdleStateEvent event = (IdleStateEvent) obj;
+            if (IdleState.WRITER_IDLE.equals(event.state())) {
+                logger.info("Closing an idle channel: {}!", atomicInteger.incrementAndGet());
+                ctx.channel().close();
+            }
+        } else {
+            super.userEventTriggered(ctx, obj);
+        }
+    }
 
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, RpcResponse response) {
--- agent-provider/src/main/java/cn/shuaijunlan/agent/provider/dubbo/RpcClientInitializer.java
+++ agent-provider/src/main/java/cn/shuaijunlan/agent/provider/dubbo/RpcClientInitializer.java
@@ -3,6 +3,9 @@ package cn.shuaijunlan.agent.provider.dubbo;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Junlan
@@ -11,6 +14,8 @@ public class RpcClientInitializer extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel socketChannel) {
         ChannelPipeline pipeline = socketChannel.pipeline();
+        //设置连接空闲时间
+        pipeline.addLast(new IdleStateHandler(0, 61, 0, TimeUnit.SECONDS));
         pipeline.addLast(new DubboRpcEncoder());
         pipeline.addLast(new DubboRpcDecoder());
         pipeline.addLast(new RpcClientHandler());
